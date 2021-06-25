c2ce9fff5da6d936070ead9f70bdcdc3a78206a6
David <david@ixulabs.com>
2021-06-25 10:20:00 -0500

    primos input

--- ejercicios/views.py
+++ ejercicios/views.py
@@ -4,6 +4,12 @@ from .utils import define_primo_proximo
 from rest_framework.response import Response
 
 class NumeroPrimoProximo(APIView):
-    def get(self, request):
-        primo = define_primo_proximo(20)
-        return Response(primo)
+    def get(self, request, *args, **kwargs):
+        
+        params = request.query_params
+
+        if 'num' in params.keys():
+            primo = define_primo_proximo(int(params['num']))
+            return Response(primo)
+
+        return Response('Error, al insertar argumento')
